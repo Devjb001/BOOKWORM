6139784c1aba05377ec4864d86208730eb756163
Devjb001 <suleelijah13@gmail.com>
2025-11-23 14:28:26 +0100

    add express limit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const app = express();
 
 job.start();
 app.use(cors());
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 const PORT = process.env.PORT;
 
 connectToDataBase();
